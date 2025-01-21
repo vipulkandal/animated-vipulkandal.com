f04cc0fbd551a5895a650bf69f281486cd3d12f1
vipulkandal <vipulkandal.gmail.com>
2025-01-21 14:27:20 +0530

    Fixing scrolling in portfolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,5 +1,96 @@
+// import { useRef } from "react";
+// import { FaGithub, FaExternalLinkAlt } from "react-icons/fa";
+// import "./portfolio.scss";
+// import { motion, useScroll, useSpring, useTransform } from "framer-motion";
+
+// const items = [
+//   {
+//     id: 1,
+//     title: "React Commerce",
+//     img: "https://images.pexels.com/photos/18073372/pexels-photo-18073372/free-photo-of-young-man-sitting-in-a-car-on-a-night-street.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+//     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+//     github: "https://github.com",
+//     liveDemo: "https://github.com",
+//   },
+
+//   {
+//     id: 2,
+//     title: "Next.js Blog",
+//     img: "https://images.pexels.com/photos/18023772/pexels-photo-18023772/free-photo-of-close-up-of-a-person-holding-a-wristwatch.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+//     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+//     github: "https://github.com",
+//     liveDemo: "https://github.com",
+//   },
+//   {
+//     id: 3,
+//     title: "Vanilla JS App",
+//     img: "https://images.pexels.com/photos/6894528/pexels-photo-6894528.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+//     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+//     github: "https://github.com",
+//     liveDemo: "https://github.com",
+//   },
+//   {
+//     id: 4,
+//     title: "Music App",
+//     img: "https://images.pexels.com/photos/18540208/pexels-photo-18540208/free-photo-of-wood-landscape-water-hill.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+//     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+//     github: "https://github.com",
+//     liveDemo: "https://github.com",
+//   },
+// ];
+
+// const Single = ({ item }) => {
+//   const ref = useRef();
+
+//   return (
+//     <section style={{ scrollSnapAlign: "start" }}>
+//       <div className="container">
+//         <div className="wrapper">
+//           <div className="imageContainer" ref={ref}>
+//             <img src={item.img} alt="" />
+//           </div>
+//           <div className="textContainer">
+//             <h2>{item.title}</h2>
+//             <p>{item.desc}</p>
+//             <div className="buttonContainer">
+//               <button>
+//                 <FaGithub />{" "}
+//                 <a href={item.github} target="_blank" rel="noreferrer">
+//                   Github Repo
+//                 </a>
+//               </button>
+//               <button>
+//                 <FaExternalLinkAlt />{" "}
+//                 <a href={item.liveDemo} target="_blank" rel="noreferrer">
+//                   Live Demo
+//                 </a>
+//               </button>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </section>
+//   );
+// };
+
+// const Portfolio = () => {
+//   const ref = useRef();
+
+//   return (
+//     <div className="portfolio" ref={ref}>
+//       <div className="progress">
+//         <h1>Portfolio</h1>
+//       </div>
+//       {items.map((item) => (
+//         <Single item={item} key={item.id} />
+//       ))}
+//     </div>
+//   );
+// };
+
+// export default Portfolio;
+
 import { useRef } from "react";
-import { FaGithub, FaExternalLinkAlt } from "react-icons/fa";
 import "./portfolio.scss";
 import { motion, useScroll, useSpring, useTransform } from "framer-motion";
 
@@ -9,64 +100,48 @@ const items = [
     title: "React Commerce",
     img: "https://images.pexels.com/photos/18073372/pexels-photo-18073372/free-photo-of-young-man-sitting-in-a-car-on-a-night-street.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
-    github: "https://github.com",
-    liveDemo: "https://github.com",
   },
-
   {
     id: 2,
     title: "Next.js Blog",
     img: "https://images.pexels.com/photos/18023772/pexels-photo-18023772/free-photo-of-close-up-of-a-person-holding-a-wristwatch.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
-    github: "https://github.com",
-    liveDemo: "https://github.com",
   },
   {
     id: 3,
     title: "Vanilla JS App",
     img: "https://images.pexels.com/photos/6894528/pexels-photo-6894528.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
-    github: "https://github.com",
-    liveDemo: "https://github.com",
   },
   {
     id: 4,
     title: "Music App",
     img: "https://images.pexels.com/photos/18540208/pexels-photo-18540208/free-photo-of-wood-landscape-water-hill.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
-    github: "https://github.com",
-    liveDemo: "https://github.com",
   },
 ];
 
 const Single = ({ item }) => {
   const ref = useRef();
 
+  const { scrollYProgress } = useScroll({
+    target: ref,
+  });
+
+  const y = useTransform(scrollYProgress, [0, 1], [-300, 300]);
+
   return (
-    <section style={{ scrollSnapAlign: "start" }}>
+    <section>
       <div className="container">
         <div className="wrapper">
           <div className="imageContainer" ref={ref}>
             <img src={item.img} alt="" />
           </div>
-          <div className="textContainer">
+          <motion.div className="textContainer" style={{ y }}>
             <h2>{item.title}</h2>
             <p>{item.desc}</p>
-            <div className="buttonContainer">
-              <button>
-                <FaGithub />{" "}
-                <a href={item.github} target="_blank" rel="noreferrer">
-                  Github Repo
-                </a>
-              </button>
-              <button>
-                <FaExternalLinkAlt />{" "}
-                <a href={item.liveDemo} target="_blank" rel="noreferrer">
-                  Live Demo
-                </a>
-              </button>
-            </div>
-          </div>
+            <button>See Demo</button>
+          </motion.div>
         </div>
       </div>
     </section>
@@ -76,10 +151,21 @@ const Single = ({ item }) => {
 const Portfolio = () => {
   const ref = useRef();
 
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["end end", "start start"],
+  });
+
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+  });
+
   return (
     <div className="portfolio" ref={ref}>
       <div className="progress">
-        <h1>Portfolio</h1>
+        <h1>Featured Works</h1>
+        <motion.div style={{ scaleX }} className="progressBar"></motion.div>
       </div>
       {items.map((item) => (
         <Single item={item} key={item.id} />
